de11c8b6780d14e4f5aa7d37b7756c24cb444f6a
pzeszko <pzeszko@gmail.com>
2017-09-13 11:13:55 +0200

    Updated start_local and changed docker plugin

--- start_local.sh
+++ start_local.sh
@@ -1,2 +1,35 @@
-mvn clean install
+
+cd account-service
+mvn clean install docker:build
+cd ..
+cd api-gateway
+mvn clean install docker:build
+cd ..
+cd clothes-service
+mvn clean install docker:build
+cd ..
+cd image-service
+mvn clean install docker:build
+cd ..
+cd order-service
+mvn clean install docker:build
+cd ..
+cd price-service
+mvn clean install docker:build
+cd ..
+cd service-registry
+mvn clean install docker:build
+cd ..
+cd shoes-service
+mvn clean install docker:build
+cd ..
+cd stock-service
+mvn clean install docker:build
+cd ..
+cd uaa-service
+mvn clean install docker:build
+cd ..
+cd user-service
+mvn clean install docker:build
+cd ..
 docker-compose up -d
